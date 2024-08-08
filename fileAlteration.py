1ab83cb8b29541a435280b4c066acd96e99d7e85
Thiago Innani <thiagoinnani@gmail.com>
2024-08-08 16:02:30 -0300

    first commit

---
+++ fileAlteration.py
@@ -0,0 +1,26 @@
+from tkinter import *
+from tkinter import filedialog as fd
+from tkinter.messagebox import showinfo
+
+import datetime
+import os
+import json
+
+class FileAlteration():
+    def __init__():
+        super().__init__()
+        
+    def selectFileExplorer():
+        print("Chegou no select")
+        directory = fd.askdirectory(
+            title='Selecione o diretório',
+            initialdir='',
+            )
+        showinfo(
+            title='Pasta selecionada',
+            message=directory
+        )
+        if directory is None:
+            return
+        
+        return directory
